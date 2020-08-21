98d1078d39e4cdca8a3b6c69d6c91f9173127cac
emilyswitzer <55185998+emilyswitzer@users.noreply.github.com>
2020-08-21 10:59:26 +0100

    Add files via upload

---
+++ SalonSYSApps.sql
@@ -0,0 +1,87 @@
+--- SalonSYS.sql - Appointments
+
+
+
+
+----------------------------------------------2016----------------------------------------------
+
+INSERT INTO appointments VALUES(001, date '2016-06-10', 2,'09:42', 2, 2, 35, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(002, date '2016-06-23', 2,'09:42', 2, 2, 35, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(003, date '2016-07-02', 3,'09:58', 2, 2, 35, 'Hazel', 0871234567);
+INSERT INTO appointments VALUES(004, date '2016-07-10', 4,'09:14', 2, 1, 35, 'Laura', 0871234567);
+INSERT INTO Appointments VALUES(005, date '2016-07-07', 5,'10:56', 1, 1, 90, 'Emily', 0832222222);
+INSERT INTO appointments VALUES(006, date '2016-07-23', 2,'09:42', 2, 2, 35, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(007, date '2016-08-01', 2,'09:42', 2, 2, 35, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(008, date '2016-09-10', 1,'09:28', 6, 5, 30, 'Rosie', 0871234567);
+INSERT INTO appointments VALUES(009, date '2016-09-11', 2,'09:56', 6, 5, 30, 'Maya', 0871234567);
+INSERT INTO Appointments VALUES(010, date '2016-11-20', 2,'09:56', 4, 4, 25, 'Mairead', 0872222222);
+INSERT INTO appointments VALUES(011, date '2016-11-21', 2,'09:44', 5, 6, 15, 'Shannon', 0871234567);
+INSERT INTO Appointments VALUES(012, date '2016-12-29', 2,'10:28', 1, 1, 90, 'Lucia', 0872222222);
+
+----------------------------------------------2017----------------------------------------------
+
+INSERT INTO appointments VALUES(013, date '2017-06-19', 1,'09:28', 1, 1, 90, 'Molly', 0871234567);
+INSERT INTO appointments VALUES(014, date '2017-06-23', 2,'09:56', 2, 2, 35, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(015, date '2017-07-02', 3,'09:28', 2, 2, 35, 'Hazel', 0871234567);
+INSERT INTO appointments VALUES(016, date '2017-07-10', 4,'09:28', 2, 1, 35, 'Laura', 0871234567);
+INSERT INTO Appointments VALUES(017, date '2017-08-07', 5,'09:56', 1, 1, 90, 'Emily', 0832222222);
+INSERT INTO appointments VALUES(018, date '2017-08-18', 1,'09:28', 2, 3, 35, 'Aileen', 0871234567);
+INSERT INTO appointments VALUES(019, date '2017-09-05', 1,'09:56', 3, 1, 60, 'Charlie', 0871234567);
+INSERT INTO appointments VALUES(020, date '2017-09-10', 1,'09:28', 6, 5, 30, 'Rosie', 0871234567);
+INSERT INTO appointments VALUES(021, date '2017-11-11', 2,'09:28', 6, 5, 30, 'Maya', 0871234567);
+INSERT INTO Appointments VALUES(022, date '2017-12-20', 2,'09:56', 4, 4, 25, 'Mairead', 0872222222);
+INSERT INTO appointments VALUES(023, date '2017-12-21', 2,'09:28', 5, 6, 15, 'Shannon', 0871234567);
+INSERT INTO Appointments VALUES(024, date '2017-12-29', 2,'09:56', 1, 1, 90, 'Lucia', 0872222222);
+
+
+----------------------------------------------2018----------------------------------------------
+
+INSERT INTO appointments VALUES(025, date '2018-06-19', 1,'09:56', 1, 1, 90, 'Molly', 0871234567);
+INSERT INTO appointments VALUES(026, date '2018-06-23', 2,'09:12', 1, 2, 90, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(027, date '2018-07-02', 3,'09:26', 1, 2, 90, 'Hazel', 0871234567);
+INSERT INTO appointments VALUES(028, date '2018-07-10', 4,'09:28', 2, 1, 35, 'Laura', 0871234567);
+INSERT INTO Appointments VALUES(029, date '2018-08-07', 5,'10:56', 1, 1, 90, 'Emily', 0832222222);
+INSERT INTO appointments VALUES(030, date '2018-08-18', 1,'09:28', 2, 3, 35, 'Aileen', 0871234567);
+INSERT INTO appointments VALUES(031, date '2018-08-05', 1,'09:56', 3, 1, 60, 'Charlie', 0871234567);
+INSERT INTO appointments VALUES(032, date '2018-09-10', 1,'09:56', 1, 5, 90, 'Rosie', 0871234567);
+INSERT INTO appointments VALUES(033, date '2018-10-11', 2,'10:11', 1, 5, 90, 'Maya', 0871234567);
+INSERT INTO Appointments VALUES(034, date '2018-11-20', 2,'09:56', 4, 4, 25, 'Mairead', 0872222222);
+INSERT INTO appointments VALUES(035, date '2018-12-21', 2,'09:28', 5, 6, 15, 'Shannon', 0871234567);
+INSERT INTO Appointments VALUES(036, date '2018-12-29', 2,'10:11', 1, 1, 90, 'Lucia', 0872222222);
+
+----------------------------------------------2019----------------------------------------------
+
+INSERT INTO appointments VALUES(037, date '2019-01-19', 1,'09:56', 1, 1, 90, 'Molly', 0871234567);
+INSERT INTO appointments VALUES(038, date '2019-01-23', 2,'09:43', 2, 2, 35, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(039, date '2019-02-02', 3,'09:58', 2, 3, 60, 'Hazel', 0871234567);
+INSERT INTO appointments VALUES(040, date '2019-02-10', 4,'09:13', 2, 1, 25, 'Laura', 0871234567);
+INSERT INTO Appointments VALUES(041, date '2019-03-07', 5,'10:56', 1, 1, 90, 'Emily', 0832222222);
+INSERT INTO appointments VALUES(042, date '2019-03-18', 1,'09:56', 1, 3, 90, 'Aileen', 0871234567);
+INSERT INTO appointments VALUES(043, date '2019-04-05', 1,'09:56', 1, 1, 90, 'Charlie', 0871234567);
+INSERT INTO appointments VALUES(044, date '2019-04-10', 1,'09:28', 6, 5, 30, 'Rosie', 0871234567);
+INSERT INTO appointments VALUES(045, date '2019-05-11', 2,'09:43', 6, 5, 30, 'Maya', 0871234567);
+INSERT INTO Appointments VALUES(046, date '2019-05-20', 2,'10:11', 1, 1, 90, 'Mairead', 0872222222);
+INSERT INTO appointments VALUES(047, date '2019-06-21', 2,'10:11', 1, 2, 90, 'Shannon', 0871234567);
+INSERT INTO Appointments VALUES(048, date '2019-06-29', 2,'10:11', 1, 3, 90, 'Lucia', 0872222222);
+INSERT INTO appointments VALUES(049, date '2019-07-19', 1,'09:56', 1, 1, 90, 'Molly', 0871234567);
+INSERT INTO appointments VALUES(050, date '2019-07-23', 1,'09:42', 3, 2, 60, 'Mary', 0861234567);
+INSERT INTO appointments VALUES(051, date '2019-08-02', 1,'09:42', 3, 2, 60, 'Hazel', 0871234567);
+INSERT INTO appointments VALUES(052, date '2019-08-10', 4,'09:13', 4, 6, 25, 'Laura', 0871234567);
+INSERT INTO Appointments VALUES(053, date '2019-09-07', 5,'10:28', 4, 4, 90, 'Emily', 0832222222);
+INSERT INTO appointments VALUES(054, date '2019-09-18', 1,'09:14', 5, 4, 35, 'Aileen', 0871234567);
+INSERT INTO appointments VALUES(055, date '2019-10-05', 1,'09:14', 5, 6, 15, 'Charlie', 0871234567);
+INSERT INTO appointments VALUES(056, date '2019-10-10', 1,'09:28', 6, 5, 30, 'Rosie', 0871234567);
+INSERT INTO appointments VALUES(057, date '2019-11-11', 2,'09:43', 6, 5, 30, 'Maya', 0871234567);
+INSERT INTO Appointments VALUES(058, date '2019-11-20', 2,'09:43', 6, 7, 25, 'Mairead', 0872222222);
+INSERT INTO appointments VALUES(059, date '2019-12-21', 3,'10:12', 3, 2, 60, 'Shannon', 0871234567);
+INSERT INTO Appointments VALUES(060, date '2019-12-29', 1,'09:56', 1, 1, 90, 'Lucia', 0872222222);
+INSERT INTO Appointments VALUES(061, date '2019-12-31', 1,'09:56', 1, 1, 90, 'Frankie', 0872222222);
+INSERT INTO Appointments VALUES(062, date '2019-12-31', 1,'09:28', 2, 2, 35, 'Anna', 0872222222);
+INSERT INTO Appointments VALUES(063, date '2019-12-31', 1,'09:28', 2, 3, 35, 'Kelly', 0872222222);
+
+----------------------------------------------2020----------------------------------------------
+
+
+INSERT INTO appointments VALUES(067, date '2020-06-05', 1,'09:28', 2, 2, 35, 'Molly', 0871234567);
+
+commit;
